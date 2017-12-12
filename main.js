dca77a42009d11e81b5cc56ab87b53aed85ec625
Pavlo Taras <pavlo.taras1985@gmail.com>
2017-12-12 17:20:07 +0200

    Added code for first task

---
+++ main.js
@@ -0,0 +1,38 @@
+function addElement(tag, listAttributes) {
+  var element = document.createElement(tag);
+  
+  for (attr in listAttributes) {
+    element.setAttribute(attr, listAttributes[attr]);
+  }
+
+  return element;
+}
+
+var form = addElement('form', { name: 'login', action: 'google.com', onSubmit: 'return validateData();'});
+form.appendChild(addElement('input', { 
+  type: 'text',
+  name: 'age',
+  placeholder: '0',
+  title: '[0-9]+',
+  pattern: '[0-9]+',
+  required: 'required'
+}));
+form.appendChild(addElement('input', {
+  type: 'text',
+  name: 'username',
+  placeholder: 'user_name',
+  title: 'user_[name]',
+  pattern: 'user_[A-Za-z0-9-]+',
+  required: 'required'
+}));
+form.appendChild(addElement('input', {
+    type: 'text',
+    name: 'date',
+    placeholder: 'dd/mm/yyyy',
+    title: 'dd/mm/yyyy',
+    pattern: '(0[1-9]|1[0-9]|2[0-9]|3[01])/(0[1-9]|1[012])/[0-9]{4}',
+    required: 'required'
+}));
+form.appendChild(addElement('input', {type: 'submit', value: 'Validate Me'}));
+
+document.body.appendChild(form);
