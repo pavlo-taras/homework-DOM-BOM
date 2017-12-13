72cc6856f46c12e355ead0da8decb2c939faed00
Pavlo Taras <pavlo.taras1985@gmail.com>
2017-12-13 09:29:22 +0200

    Added validation date fields

--- main.js
+++ main.js
@@ -8,7 +8,8 @@ function addElement(tag, listAttributes) {
   return element;
 }
 
-var form = addElement('form', { name: 'login', action: 'google.com', onSubmit: 'return validateData();'});
+var form = addElement('form', { name: 'login', action: 'google.com'/*, onSubmit: 'return validateData();'*/});
+
 form.appendChild(addElement('input', { 
   type: 'text',
   name: 'age',
@@ -17,6 +18,7 @@ form.appendChild(addElement('input', {
   pattern: '[0-9]+',
   required: 'required'
 }));
+
 form.appendChild(addElement('input', {
   type: 'text',
   name: 'username',
@@ -25,6 +27,7 @@ form.appendChild(addElement('input', {
   pattern: 'user_[A-Za-z0-9-]+',
   required: 'required'
 }));
+
 form.appendChild(addElement('input', {
     type: 'text',
     name: 'date',
@@ -33,6 +36,19 @@ form.appendChild(addElement('input', {
     pattern: '(0[1-9]|1[0-9]|2[0-9]|3[01])/(0[1-9]|1[012])/[0-9]{4}',
     required: 'required'
 }));
+
 form.appendChild(addElement('input', {type: 'submit', value: 'Validate Me'}));
 
 document.body.appendChild(form);
+
+form.addEventListener('submit', function(event) {
+    
+  var dateNow = new Date(); 
+  var inputDateList = this.elements.date.value.split('/');
+  var inputDate = new Date(inputDateList[2], inputDateList[1] - 1, inputDateList[0]);
+
+  if (dateNow.toDateString() !== inputDate.toDateString()) {
+    alert('Invalid data. Please enter current date');
+    event.preventDefault();
+  }
+}, false);
